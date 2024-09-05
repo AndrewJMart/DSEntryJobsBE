68fd5aca0e7e368a7db5f8ec7cf2ba4a820a91f6
AndrewJMart <amart89531@gmail.com>
2024-09-04 23:50:53 -0700

    Allow Any Origin For Testing Purposes

--- src/api/server.py
+++ src/api/server.py
@@ -25,10 +25,10 @@ origins = ["https://potion-exchange.vercel.app"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["GET", "OPTIONS"],
-    allow_headers=["*"],
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
 app.include_router(jobs.router)
